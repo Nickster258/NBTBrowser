30d291538a0c40575bab0d66370f1b6bc62c4c4e
Nicholas Stonecipher <nickster258@users.noreply.github.com>
2020-07-11 19:39:18 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-    name="NBTBrowser",
+    name="nbtbrowser",
     version="0.0.1",
     author="Nicholas Stonecipher",
     author_email="nickster258@users.noreply.github.com",
-    packages=find_packages(),
     py_modules=['nbtbrowser'],
-    entry_points='''
-        [console_scripts]
-        nbtbrowser=nbtbrowser:main
-    ''',
     install_requires=[
         'Click',
         'nbtlib~=1.7.0',
         'tabulate~=0.8.7'
-    ]
+    ],
+    entry_points='''
+        [console_scripts]
+        nbtbrowser=nbtbrowser:main
+    '''
 )
